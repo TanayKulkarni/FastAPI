e80b64f34015117306ce05412d6d906fd4e5df8d
Tanay Kulkarni <tanaykulkarni@Tanays-MacBook-Air.local>
2021-05-01 22:21:40 -0400

    html

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
 
 app = FastAPI()
 
@@ -6,3 +7,18 @@ app = FastAPI()
 @app.get("/")
 async def base():
     return {"Tanay": "Kulkarni"}
+
+@app.get("/html",tags=['html'])
+async def root():
+    html_content = """
+    <html>
+        <head>
+           <title>FastAPI</title>
+        </head>
+        <body><marquee>"""+'app bangaaya be!!!!'+"""</marquee>
+        
+        </body>
+    </html>
+    """
+
+    return HTMLResponse(content=html_content,status_code=200)
