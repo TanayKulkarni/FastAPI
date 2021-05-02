60f13e3d033ec5c7d424f94d6ddf5fc67aa572fe
Tanay Kulkarni <tanaykulkarni@Tanays-MacBook-Air.local>
2021-05-02 14:59:33 -0400

    index.html

--- app.py
+++ app.py
@@ -10,15 +10,4 @@ async def base():
 
 @app.get("/html",tags=['html'])
 async def root():
-    html_content = """
-    <html>
-        <head>
-           <title>FastAPI</title>
-        </head>
-        <body><marquee>"""+'app bangaaya be!!!!'+"""</marquee>
-        
-        </body>
-    </html>
-    """
-
-    return HTMLResponse(content=html_content,status_code=200)
+    return HTMLResponse(content='index.html',status_code=200)
